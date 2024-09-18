972a326cbecf6f5e4af63ad4c9d1ae10c42d9298
EWehmeyer97 <ewehmeyer97@gmail.com>
2024-09-18 00:45:30 -0400

    Blank window - most likely need to setup to ignore build folder

---
+++ build/CMakeFiles/opengl_window.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_window.dir/src/glad.c.o"
+  "CMakeFiles/opengl_window.dir/src/glad.c.o.d"
+  "CMakeFiles/opengl_window.dir/src/main.cpp.o"
+  "CMakeFiles/opengl_window.dir/src/main.cpp.o.d"
+  "opengl_window"
+  "opengl_window.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl_window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
